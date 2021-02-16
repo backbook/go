a75965c8e523f3344520ee73797b79532e128850
sujy <sujy@akulaku.com>
2021-02-16 21:33:53 +0800

    add go code

--- src/main/ObjMain.go
+++ src/main/ObjMain.go
@@ -2,40 +2,18 @@ package main
 
 import (
 	"fmt"
+	"oop"
 )
-
-type Person struct {
-	name string
-	age  int
-	sex  byte
-}
-
-type Student struct {
-	Person  //只有类型 ，没有名字，匿名字段，相当于继承了Person的 ,匿名字段
-	name    string
-	score   int
-	address string
-}
 
 func main() {
 
-	var s1 Student = Student{Person{"张三", 20, 0}, "何中", 99, "张家界"}
+	var s1 = oop.Student{}
+	s1.SetName("张三")
 	//%+v 显示更加详细的
-	fmt.Printf("s1 = %+v", s1)
+	s1.GetName()
 
-	fmt.Println()
 	//指定成员初始化，没有初始化的自动赋值为0
-	s2 := Student{Person: Person{name: "李四"}, score: 100} //使用这个方式可以灵活点
+	s2 := oop.Student{} //使用这个方式可以灵活点
 	fmt.Printf("s2 = %+v", s2)
-
-	fmt.Println()
-	fmt.Println(s2.name)
-	var s3 Student
-	s3.name = "王五"
-	s3.address = "湖头"
-	s3.score = 100
-	s3.sex = 0
-	s3.Person.name = "杜悦"
-	fmt.Printf("s3 = %+v", s3)
 
 }
--- src/oop/entity.go
+++ src/oop/entity.go
@@ -1,4 +1,6 @@
-package obj
+package oop
+
+import "fmt"
 
 type Person struct {
 	name string
@@ -10,4 +12,12 @@ type Student struct {
 	Person  //只有类型，没有名字，匿名字段，相当于继承了Person的 ,匿名字段
 	score   int
 	address string
+}
+
+func (student *Student) SetName(name string) {
+	student.name = name
+}
+
+func (student *Student) GetName() {
+	fmt.Println(student.name)
 }

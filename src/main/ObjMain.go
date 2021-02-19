5c4a566b3ea266e1c1067dc94510b68ed6704fc5
backbook <76886049@qq.com>
2021-02-19 15:12:09 +0800

    add go code

--- src/main/ObjMain.go
+++ src/main/ObjMain.go
@@ -7,13 +7,11 @@ import (
 
 func main() {
 
-	var s1 = oop.Student{}
+	var s1 oop.Student = oop.Student{}
 	s1.SetName("张三")
 	//%+v 显示更加详细的
-	s1.GetName()
-
-	//指定成员初始化，没有初始化的自动赋值为0
-	s2 := oop.Student{} //使用这个方式可以灵活点
-	fmt.Printf("s2 = %+v", s2)
-
+	name := s1.GetName()
+	fmt.Println(name)
+	//匿名结构体
+	oop.Amonymous()
 }
--- src/oop/entity.go
+++ src/oop/entity.go
@@ -10,6 +10,7 @@ type Person struct {
 
 type Student struct {
 	Person  //只有类型，没有名字，匿名字段，相当于继承了Person的 ,匿名字段
+	name    string
 	score   int
 	address string
 }
@@ -18,6 +19,15 @@ func (student *Student) SetName(name string) {
 	student.name = name
 }
 
-func (student *Student) GetName() {
-	fmt.Println(student.name)
+func (student *Student) GetName() string {
+	return student.name
+}
+
+func Amonymous() {
+	//匿名结构体
+	var s struct {
+		name string
+	}
+	s.name = "王五"
+	fmt.Print(s.name)
 }
